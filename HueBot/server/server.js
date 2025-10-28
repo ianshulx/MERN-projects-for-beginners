525072ef57ee8466b82c0a90076b236b43bf80ce
Anurag Adhikari <anuragadhikari5276@gmail.com>
2025-10-28 11:45:08 +0545

    Define palette routes

--- HueBot/server/server.js
+++ HueBot/server/server.js
@@ -15,6 +15,7 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", require("./routes/auth"));
+app.use("/api/palettes", require("./routes/palette"));
 
 // Status check endpoint
 app.get("/", (req, res) => {

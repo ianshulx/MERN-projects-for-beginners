d8241b16fcba4f753baa6cf1092560cd16905d0d
NovA676 <114248066+NovA676@users.noreply.github.com>
2025-10-29 05:30:08 +0530

    added a template for food_delivery_app for now

---
+++ Food_Delivery_App/client/src/pages/SignUp.jsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import OAuth from "../components/OAuth";
+
+const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+      const response = await fetch("api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json();
+      setLoading(false);
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+      navigate("/signin");
+    } catch (error) {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-5">
+        <input
+          type="text"
+          placeholder="Username"
+          id="username"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          id="email"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="bg-slate-100 p-3 rounded-lg"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className="bg-red-500 rounded-lg p-3 uppercase hover:opacity-80 disabled:opacity-60"
+        >
+          {loading ? "Loading..." : "Sign Up"}
+        </button>
+        <OAuth/>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Have an account?</p>
+        <Link to="/signin">
+          <span className="text-blue-500">Sign In</span>
+        </Link>
+      </div>
+      <p className="text-red-700 mt-5">{error && "Something went wrong!"}</p>
+    </div>
+  );
+};
+
+export default SignUp;
